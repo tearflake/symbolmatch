5f6955817ce9a21b2d08cce7b0f41f1e2c1978f8
tearflake <45128428+tearflake@users.noreply.github.com>
2025-09-18 11:46:24 +0200

    Add files via upload

--- playground/examples.js
+++ playground/examples.js
@@ -12,6 +12,7 @@ examples = {
             (ATOM <number>)))
             
     (RULE <number> (MUL <digit> (STAR <digit>)))
+    
     (RULE <digit> (ADD "1" "2" "3" "4" "5" "6" "7" "8" "9" "0")))
 `,
 "arith-input":
@@ -55,6 +56,7 @@ examples = {
             (ATOM <number>)))
             
     (RULE <number> (MUL <digit> (STAR <digit>)))
+    
     (RULE <digit> (ADD "1" "2" "3" "4" "5" "6" "7" "8" "9" "0")))
 `,
 
